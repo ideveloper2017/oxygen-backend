4c2be7094bc8c971c3cce07628ff2e1198e144ce
iDeveloper <bahrom1982@gmail.com>
2023-09-06 12:11:19 +0500

    Commit

--- src/entity/sales.entity.ts
+++ src/entity/sales.entity.ts
@@ -5,19 +5,19 @@ import { Users } from './users.entity';
 
 @Entity('Sales')
 export class Sales extends Model {
-  @ManyToOne((type) => Users, users => users.sales)
+  @ManyToOne((type) => Users, (users) => users.sales)
   @JoinColumn({ name: 'user_id' })
   users: Users;
 
   @Column()
-  user_id: number
+  user_id: number;
 
-  @ManyToOne((type) => Clients, clients => clients.sales)
+  @ManyToOne((type) => Clients, (clients) => clients.sales)
   @JoinColumn({ name: 'client_id' })
   clients: Clients;
 
   @Column()
-  client_id: number
+  client_id: number;
 
   @Column()
   total_price: number;
