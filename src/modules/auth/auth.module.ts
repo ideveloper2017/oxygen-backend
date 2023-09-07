56659472189d95b5b5f8b73d16113e027ea6a679
iDeveloper <bahrom1982@gmail.com>
2023-09-07 09:59:53 +0500

    Commit

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,8 @@ import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from '../../constants';
 import { AuthService } from '../../service/auth.service';
-import { APP_GUARD } from '@nestjs/core';
-
 import { AuthController } from '../../controller/auth/auth.controller';
-import { AuthGuard } from '../../controller/auth/AuthGuard';
+
 
 @Module({
   imports: [
@@ -17,13 +15,7 @@ import { AuthGuard } from '../../controller/auth/AuthGuard';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [
-    AuthService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-  ],
+  providers: [],
   controllers: [AuthController],
   exports: [AuthService],
 })
