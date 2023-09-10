178b38c462186830ff01568924834c2a75bd808e
apple <bahrom1982@gmail.com>
2023-09-10 18:18:45 +0500

    commit

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -33,7 +33,7 @@ export class UsersController {
   @Delete('/delete')
   deleteUsers(@Body() userid:number[]) {
 
-    return userid;
+    console.log(userid);
     // return this.usersService.deleteUsers(userid).then((data)=>{
     //   return data.affected != 0?{success:true,message:'Deleted is record!!!'} :{success:false,message:'not deleted!!!'}
     // }).catch((error)=>{
